c6e0df4fc56200a391fe045cecac5aeff460c1cd
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2019-10-10 18:44:26 -0500

    Finished (code built with Visual Studio)

--- heap.h
+++ heap.h
@@ -1,8 +1,64 @@
-//
-// Created by rudri on 10/10/2019.
-//
+#pragma once
+#include<vector>
+#include<fstream>
+#include<iostream>
+#include<iterator>
+#include<ostream>
 
-#ifndef CS1103_PC2_201902_HEAP_H
-#define CS1103_PC2_201902_HEAP_H
 
-#endif //CS1103_PC2_201902_HEAP_H
+using namespace std;
+
+//template class with functions
+template<class T>
+class heap {
+public:
+	vector<T> Store;
+
+	auto get_max() {
+		auto max = Store[0];
+		for(int i = 1; i< size(); i++){
+			if (max < Store[i]) {
+				max = Store[i];
+			}
+		}
+		return max;
+	}
+
+	void insert(T items) {
+		Store.push_back(items);
+	}
+
+	int size() {
+		int size;
+		size= Store.size();
+		return size;
+	}
+	void clear() {
+		Store.resize(0);
+	}
+
+//variadic Insert
+template<typename...Types>
+	void insert(Types... args) {
+		int a[] = { 0,(Store.push_back(args), 0)... };
+		static_cast<void>(a);
+	}
+};
+
+
+
+//additional generic functions
+
+//load from
+template<typename A, typename T>
+void load_from(A in, heap<T>& hp) {
+	hp.clear();	
+	ifstream file(in);
+	istream_iterator<T> start(file), end;
+	vector<T> temp(start, end);
+}
+
+//input and output overload
+template<typename T>
+ostream& operator<< (ostream& out, heap<T>& heap) {	out << "{";	for (int i = 0; i < heap.size(); i++) {		out << heap.Store[i];		if (i != heap.size() - 1) {			out << ", ";		}	}	out << "}" << endl;	return out;}template<typename T>istream& operator>> (istream& in, heap<T>& heap) {	T input;	in>>input;	heap.insert(input);	return in;}
+
--- main.cpp
+++ main.cpp
@@ -6,22 +6,30 @@ using namespace std;
 int main()
 {
     heap<int> h;
-    load_from("data.txt", h);
-    cout << "heap" << endl;
+    /*load_from("data.txt", h);
+    cout << "heap" << endl;*/
     cout << h;
     h.insert(10);
     h.insert(20);
     h.insert(40);
     h.insert(50);
     h.insert(10);
+	h.insert(12,21,12,31,312,32,2);
+	auto size = h.size();
+	cout << size << endl;
+	auto max = h.get_max();
+	cout << max << endl;
+	cout << h.Store[8];
+
     cout << "heap" << endl;
     cout << h;
     cout << "heap" << endl;
     cout << h;
     cin >> h;
+	cout << h;
     cout << "heap" << endl;
     cout << h;
-    insert(h, 100, 200);
+	insert(h, 100, 200);
     cout << "heap" << endl;
     cout << h;
     load_from("data.txt", h);
